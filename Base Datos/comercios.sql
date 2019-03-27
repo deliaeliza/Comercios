26ae53ffbcb38e20e30a33746fca13049a773631
JonathanVM <33185089+JonathanVM@users.noreply.github.com>
2019-03-27 00:35:45 -0600

    cambios sql

--- Base Datos/comercios.sql
+++ Base Datos/comercios.sql
@@ -1,9 +1,5 @@
 -- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
 -- Schema comercios
 -- -----------------------------------------------------
@@ -19,18 +15,28 @@ USE `comercios` ;
 -- Table `comercios`.`Usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `comercios`.`Usuarios` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `tipo` TINYINT NOT NULL,
+  `id` INT NOT NULL,
+  `tipo` TINYINT(5) NOT NULL,
   `codigoRestablecer` VARCHAR(45) NULL,
   `correo` VARCHAR(45) NOT NULL,
-  `usuario` VARCHAR(60) NOT NULL,
+  `usuario` VARCHAR(45) NOT NULL,
   `contrasena` VARCHAR(45) NOT NULL,
   `estado` TINYINT(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `correo_UNIQUE` (`correo`))
 ENGINE = InnoDB;
-
-
+ALTER TABLE `comercios`.`Usuarios` ADD CONSTRAINT `usuario_ck_tipo` CHECK(tipo in(0, 1, 2, 3));
+DELIMITER //
+CREATE TRIGGER TcontrasenaActualizada BEFORE UPDATE on `comercios`.`Usuarios` FOR EACH ROW
+BEGIN
+	DECLARE contrasenaActual VARCHAR(45);
+	SELECT contrasena INTO contrasenaActual FROM comercios.Usuarios WHERE comercios.Usuarios.id = NEW.id;
+	IF(contrasenaActual <> NEW.contrasena) THEN 
+		UPDATE comercios.Usuarios SET codigoRestablecer = NULL WHERE comercios.Usuarios.id = NEW.id;
+	END IF;
+END;
+//
+DELIMITER ;
 -- -----------------------------------------------------
 -- Table `comercios`.`Comercio`
 -- -----------------------------------------------------
@@ -47,11 +53,20 @@ CREATE TABLE IF NOT EXISTS `comercios`.`Comercio` (
   CONSTRAINT `comercios_fk_id`
     FOREIGN KEY (`idUsuario`)
     REFERENCES `comercios`.`Usuarios` (`id`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
+DELIMITER //
+CREATE PROCEDURE PAregistrarComercio(IN Ptipo TINYINT(5), IN Pcorreo VARCHAR(45), IN Pusuario VARCHAR(45), 
+IN Pcontrasena VARCHAR(45), IN Ptelefono INT, IN Pdescripcion VARCHAR(500), IN Pubicacion VARCHAR(200), IN Pcategoria VARCHAR(50))
+BEGIN
+	DECLARE idC INT;
+	SET idC = (SELECT COUNT(id) FROM comercios.Usuarios) + 1;
+	INSERT INTO comercios.Usuarios(id, tipo, correo, usuario, contrasena, estado) VALUES (idC, Ptipo, Pcorreo, Pusuario, Pcontrasena, TRUE);
+	INSERT INTO comercios.Comercio(idUsuario, telefono, verificado, descripcion, ubicacion, categoria) VALUES (idC, Ptelefono, FALSE, Pdescripcion, Pubicacion, Pcategoria);
+END;
+//
+DELIMITER ;
 -- -----------------------------------------------------
 -- Table `comercios`.`UsuariosEstandar`
 -- -----------------------------------------------------
@@ -64,10 +79,18 @@ CREATE TABLE IF NOT EXISTS `comercios`.`UsuariosEstandar` (
   CONSTRAINT `estandar_fk_id`
     FOREIGN KEY (`idUsuario`)
     REFERENCES `comercios`.`Usuarios` (`id`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
+DELIMITER //
+CREATE PROCEDURE PAregistrarUsuarioEstandar(IN Ptipo TINYINT(5), IN Pcorreo VARCHAR(45), IN Pusuario VARCHAR(45), IN Pcontrasena VARCHAR(45), IN PfechaNac DATE)
+BEGIN
+	DECLARE idE INT;
+	SET idE = (SELECT COUNT(id) FROM comercios.Usuarios) + 1;
+	INSERT INTO comercios.Usuarios(id, tipo, correo, usuario, contrasena, estado) VALUES (idE, Ptipo, Pcorreo, Pusuario, Pcontrasena, TRUE);
+	INSERT INTO comercios.UsuariosEstandar(idUsuario, fechaNac) VALUES (idE, PfechaNac);
+END;
+//
+DELIMITER ;
 
 -- -----------------------------------------------------
 -- Table `comercios`.`Administradores`
@@ -82,10 +105,18 @@ CREATE TABLE IF NOT EXISTS `comercios`.`Administradores` (
   CONSTRAINT `admin_fk_id`
     FOREIGN KEY (`idUsuario`)
     REFERENCES `comercios`.`Usuarios` (`id`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
+DELIMITER //
+CREATE PROCEDURE PAregistrarAdministrador(IN Ptipo TINYINT(5), IN Pcorreo VARCHAR(45), IN Pusuario VARCHAR(45), IN Pcontrasena VARCHAR(45), IN Ptelefono INT)
+BEGIN
+	DECLARE idA INT;
+	SET idA = (SELECT COUNT(id) FROM comercios.Usuarios) + 1;
+	INSERT INTO comercios.Usuarios(id, tipo, correo, usuario, contrasena, estado) VALUES (idA, Ptipo, Pcorreo, Pusuario, Pcontrasena, TRUE);
+	INSERT INTO comercios.Administradores(idUsuario, telefono) VALUES (idA, Ptelefono);
+END;
+//
+DELIMITER ;
 
 -- -----------------------------------------------------
 -- Table `comercios`.`Calificaciones`
@@ -99,12 +130,10 @@ CREATE TABLE IF NOT EXISTS `comercios`.`Calificaciones` (
   CONSTRAINT `calificacion_fk_idComercio`
     FOREIGN KEY (`idComercio`)
     REFERENCES `comercios`.`Comercio` (`idUsuario`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `calificacion_fk_idUsuario`
     FOREIGN KEY (`idUsuarioE`)
     REFERENCES `comercios`.`UsuariosEstandar` (`idUsuario`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
@@ -132,7 +161,6 @@ CREATE TABLE IF NOT EXISTS `comercios`.`Secciones` (
   CONSTRAINT `seccion_fk_idComercio`
     FOREIGN KEY (`idComercio`)
     REFERENCES `comercios`.`Comercio` (`idUsuario`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
@@ -150,7 +178,6 @@ CREATE TABLE IF NOT EXISTS `comercios`.`Productos` (
   CONSTRAINT `producto_fk_comercio`
     FOREIGN KEY (`idComercio`)
     REFERENCES `comercios`.`Comercio` (`idUsuario`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
@@ -166,9 +193,7 @@ CREATE TABLE IF NOT EXISTS `comercios`.`ProductoImagenes` (
   INDEX `imagenes_fk_producto_idx` (`idProducto`),
   CONSTRAINT `imagenes_fk_producto`
     FOREIGN KEY (`idProducto`)
-    REFERENCES `comercios`.`Productos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `comercios`.`Productos` (`id`))
 ENGINE = InnoDB;
 
 
@@ -182,17 +207,11 @@ CREATE TABLE IF NOT EXISTS `comercios`.`SeccionesProductos` (
   INDEX `producto_fk_seccion_idx` (`idProducto`),
   CONSTRAINT `seccion_fk_producto`
     FOREIGN KEY (`idSeccion`)
-    REFERENCES `comercios`.`Secciones` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `comercios`.`Secciones` (`id`),
   CONSTRAINT `producto_fk_seccion`
     FOREIGN KEY (`idProducto`)
-    REFERENCES `comercios`.`Productos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `comercios`.`Productos` (`id`))
 ENGINE = InnoDB;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
