6d8fb6ff408cec0cf4f47a604c09ecbdd7a6aef5
JonathanVM <33185089+JonathanVM@users.noreply.github.com>
2019-03-27 13:26:02 -0600

    constructores

--- comerciosCR/app/src/main/java/Modelo/Calificacion.java
+++ comerciosCR/app/src/main/java/Modelo/Calificacion.java
@@ -3,4 +3,23 @@ package Modelo;
 public class Calificacion {
     private int calificacion;
     private Comercio comercio;
+
+    public Calificacion(int calificacion, Comercio comercio) {
+        this.calificacion = calificacion;
+        this.comercio = comercio;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public Comercio getComercio() {
+        return comercio;
+    }
+    public void setComercio(Comercio comercio) {
+        this.comercio = comercio;
+    }
 }
