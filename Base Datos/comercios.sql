46e3e1b6481b1d2021c6e3c2f5e8d650bf0c5a26
Elizabeth Hernandez <deliaeliza72@gmail.com>
2019-04-14 12:14:24 -0600

    Update comercios.sql

--- Base Datos/comercios.sql
+++ Base Datos/comercios.sql
@@ -3,18 +3,18 @@
 -- -----------------------------------------------------
 -- Schema comercios
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `COMERCIOSCR` ;
+DROP SCHEMA IF EXISTS `comercioscr` ;
 
 -- -----------------------------------------------------
 -- Schema comercios
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `COMERCIOSCR` DEFAULT CHARACTER SET utf8 ;
-USE `COMERCIOSCR` ;
+CREATE SCHEMA IF NOT EXISTS `comercioscr` DEFAULT CHARACTER SET utf8 ;
+USE `comercioscr` ;
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`Categorias`
+-- Table `comercioscr`.`Categorias`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Categorias`(
+CREATE TABLE IF NOT EXISTS `comercioscr`.`Categorias`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `nombre` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`id`),
@@ -23,9 +23,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`Usuarios`
+-- Table `comercioscr`.`Usuarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Usuarios` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`Usuarios` (
   `id` INT NOT NULL,
   `tipo` TINYINT(5) NOT NULL,
   `correo` VARCHAR(45) NOT NULL,
@@ -37,41 +37,41 @@ CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Usuarios` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`RestablecerPWD`
+-- Table `comercioscr`.`RestablecerPWD`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`RestablecerPWD` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`RestablecerPWD` (
     `idUsuario` INT NOT NULL,
     `codigo`    VARCHAR(45) NOT NULL,
     PRIMARY KEY (`idUsuario`),
     INDEX `retablecer_fk_usuario_idx` (`idUsuario`),
     CONSTRAINT `restablecer_fk_usuarios`
     FOREIGN KEY (`idUsuario`)
-    REFERENCES `COMERCIOSCR`.`Usuarios` (`id`)
+    REFERENCES `comercioscr`.`Usuarios` (`id`)
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-ALTER TABLE `COMERCIOSCR`.`Usuarios` ADD CONSTRAINT `usuario_ck_tipo` CHECK(tipo in(0, 1, 2, 3));
+ALTER TABLE `comercioscr`.`Usuarios` ADD CONSTRAINT `usuario_ck_tipo` CHECK(tipo in(0, 1, 2, 3));
 
 DELIMITER //
-CREATE TRIGGER TcontrasenaActualizada BEFORE UPDATE on COMERCIOSCR.Usuarios FOR EACH ROW
+CREATE TRIGGER TcontrasenaActualizada BEFORE UPDATE on comercioscr.Usuarios FOR EACH ROW
 BEGIN
 	DECLARE contrasenaActual VARCHAR(45);
     DECLARE idRestablecer INT;
-	SELECT contrasena INTO contrasenaActual FROM COMERCIOSCR.Usuarios WHERE COMERCIOSCR.Usuarios.id = NEW.id;
-    SELECT idUsuario INTO idRestablecer FROM COMERCIOSCR.RestablecerPWD WHERE idUsuario = NEW.id;
+	SELECT contrasena INTO contrasenaActual FROM comercioscr.Usuarios WHERE comercioscr.Usuarios.id = NEW.id;
+    SELECT idUsuario INTO idRestablecer FROM comercioscr.RestablecerPWD WHERE idUsuario = NEW.id;
 	IF(contrasenaActual <> NEW.contrasena) AND(idRestablecer IS NOT NULL) THEN
-		DELETE FROM COMERCIOSCR.RestablecerPWD WHERE idUsuario = NEW.id;
+		DELETE FROM comercioscr.RestablecerPWD WHERE idUsuario = NEW.id;
 	END IF;
 END;
 //
 DELIMITER ;
 
 DELIMITER //
-CREATE TRIGGER TusuarioSuper BEFORE INSERT ON COMERCIOSCR.Usuarios FOR EACH ROW
+CREATE TRIGGER TusuarioSuper BEFORE INSERT ON comercioscr.Usuarios FOR EACH ROW
 BEGIN
 	DECLARE cantidad INT;
 	IF (NEW.tipo = 0) THEN
-		SELECT COUNT(id) INTO cantidad FROM COMERCIOSCR.Usuarios WHERE COMERCIOSCR.Usuarios.tipo = 0;
+		SELECT COUNT(id) INTO cantidad FROM comercioscr.Usuarios WHERE comercioscr.Usuarios.tipo = 0;
 		IF(cantidad >= 1) THEN 
 			SIGNAL SQLSTATE '45000' SET message_text = 'Ya existe el usuario de tipo super';
 		END IF;
@@ -81,9 +81,9 @@ END;
 DELIMITER ;
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`Comercios`
+-- Table `comercioscr`.`Comercios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Comercios` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`Comercios` (
   `idUsuario` INT NOT NULL,
   `telefono` INT NULL,
   `idCategoria` INT NOT NULL,
@@ -97,11 +97,11 @@ CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Comercios` (
   PRIMARY KEY (`idUsuario`),
   CONSTRAINT `comercios_fk_id`
     FOREIGN KEY (`idUsuario`)
-    REFERENCES `COMERCIOSCR`.`Usuarios` (`id`)
+    REFERENCES `comercioscr`.`Usuarios` (`id`)
     ON UPDATE NO ACTION,
   CONSTRAINT `comercios_fk_cat`
     FOREIGN KEY (`idCategoria`)
-    REFERENCES `COMERCIOSCR`.`Categorias` (`id`)
+    REFERENCES `comercioscr`.`Categorias` (`id`)
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
@@ -110,16 +110,16 @@ CREATE PROCEDURE PAregistrarComercio(IN Ptipo TINYINT(5), IN Pcorreo VARCHAR(45)
 IN Pcontrasena VARCHAR(45), IN Ptelefono INT, IN Pdescripcion VARCHAR(500), IN Pubicacion VARCHAR(200), IN Pcategoria VARCHAR(50), IN PUrlImagen VARCHAR(100))
 BEGIN
 	DECLARE idC INT;
-	SET idC = (SELECT COUNT(id) FROM COMERCIOSCR.Usuarios) + 1;
-	INSERT INTO COMERCIOSCR.Usuarios(id, tipo, correo, usuario, contrasena, estado) VALUES (idC, Ptipo, Pcorreo, Pusuario, Pcontrasena, TRUE);
-	INSERT INTO COMERCIOSCR.Comercio(idUsuario, telefono, verificado, descripcion, ubicacion, categoria, urlImagen) VALUES (idC, Ptelefono, FALSE, Pdescripcion, Pubicacion, Pcategoria, PUrlImagen);
+	SET idC = (SELECT COUNT(id) FROM comercioscr.Usuarios) + 1;
+	INSERT INTO comercioscr.Usuarios(id, tipo, correo, usuario, contrasena, estado) VALUES (idC, Ptipo, Pcorreo, Pusuario, Pcontrasena, TRUE);
+	INSERT INTO comercioscr.Comercio(idUsuario, telefono, verificado, descripcion, ubicacion, categoria, urlImagen) VALUES (idC, Ptelefono, FALSE, Pdescripcion, Pubicacion, Pcategoria, PUrlImagen);
 END;
 //
 DELIMITER ;
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`UsuariosEstandar`
+-- Table `comercioscr`.`UsuariosEstandar`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`UsuariosEstandar` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`UsuariosEstandar` (
   `idUsuario` INT NOT NULL,
   `fechaNac` DATE NOT NULL,
   INDEX `estandar_fk_id_idx` (`idUsuario`),
@@ -127,24 +127,24 @@ CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`UsuariosEstandar` (
   PRIMARY KEY (`idUsuario`),
   CONSTRAINT `estandar_fk_id`
     FOREIGN KEY (`idUsuario`)
-    REFERENCES `COMERCIOSCR`.`Usuarios` (`id`)
+    REFERENCES `comercioscr`.`Usuarios` (`id`)
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 DELIMITER //
 CREATE PROCEDURE PAregistrarUsuarioEstandar(IN Ptipo TINYINT(5), IN Pcorreo VARCHAR(45), IN Pusuario VARCHAR(45), IN Pcontrasena VARCHAR(45), IN PfechaNac DATE)
 BEGIN
 	DECLARE idE INT;
-	SET idE = (SELECT COUNT(id) FROM COMERCIOSCR.Usuarios) + 1;
-	INSERT INTO COMERCIOSCR.Usuarios(id, tipo, correo, usuario, contrasena, estado) VALUES (idE, Ptipo, Pcorreo, Pusuario, Pcontrasena, TRUE);
-	INSERT INTO COMERCIOSCR.UsuariosEstandar(idUsuario, fechaNac) VALUES (idE, PfechaNac);
+	SET idE = (SELECT COUNT(id) FROM comercioscr.Usuarios) + 1;
+	INSERT INTO comercioscr.Usuarios(id, tipo, correo, usuario, contrasena, estado) VALUES (idE, Ptipo, Pcorreo, Pusuario, Pcontrasena, TRUE);
+	INSERT INTO comercioscr.UsuariosEstandar(idUsuario, fechaNac) VALUES (idE, PfechaNac);
 END;
 //
 DELIMITER ;
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`Administradores`
+-- Table `comercioscr`.`Administradores`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Administradores` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`Administradores` (
   `idUsuario` INT NOT NULL,
   `codigoArea` INT NOT NULL,
   `telefono` INT NOT NULL,
@@ -154,24 +154,24 @@ CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Administradores` (
   PRIMARY KEY (`idUsuario`),
   CONSTRAINT `admin_fk_id`
     FOREIGN KEY (`idUsuario`)
-    REFERENCES `COMERCIOSCR`.`Usuarios` (`id`)
+    REFERENCES `comercioscr`.`Usuarios` (`id`)
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 DELIMITER //
 CREATE PROCEDURE PAregistrarAdministrador(IN Ptipo TINYINT(5), IN Pcorreo VARCHAR(45), IN Pusuario VARCHAR(45), IN Pcontrasena VARCHAR(45), IN Ptelefono INT, IN PCodigoArea INT)
 BEGIN
 	DECLARE idA INT;
-	SET idA = (SELECT COUNT(id) FROM COMERCIOSCR.Usuarios) + 1;
-	INSERT INTO COMERCIOSCR.Usuarios(id, tipo, correo, usuario, contrasena, estado) VALUES (idA, Ptipo, Pcorreo, Pusuario, Pcontrasena, TRUE);
-	INSERT INTO COMERCIOSCR.Administradores(idUsuario, telefono, codigoArea) VALUES (idA, Ptelefono, PCodigoArea);
+	SET idA = (SELECT COUNT(id) FROM comercioscr.Usuarios) + 1;
+	INSERT INTO comercioscr.Usuarios(id, tipo, correo, usuario, contrasena, estado) VALUES (idA, Ptipo, Pcorreo, Pusuario, Pcontrasena, TRUE);
+	INSERT INTO comercioscr.Administradores(idUsuario, telefono, codigoArea) VALUES (idA, Ptelefono, PCodigoArea);
 END;
 //
 DELIMITER ;
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`Calificaciones`
+-- Table `comercioscr`.`Calificaciones`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Calificaciones` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`Calificaciones` (
   `idComercio` INT NOT NULL,
   `idUsuarioE` INT NOT NULL,
   `calificacion` TINYINT(10) NOT NULL,
@@ -180,19 +180,19 @@ CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Calificaciones` (
   PRIMARY KEY (`idComercio`, `idUsuarioE`),
   CONSTRAINT `calificacion_fk_idComercio`
     FOREIGN KEY (`idComercio`)
-    REFERENCES `COMERCIOSCR`.`Comercios` (`idUsuario`)
+    REFERENCES `comercioscr`.`Comercios` (`idUsuario`)
     ON UPDATE NO ACTION,
   CONSTRAINT `calificacion_fk_idUsuario`
     FOREIGN KEY (`idUsuarioE`)
-    REFERENCES `COMERCIOSCR`.`UsuariosEstandar` (`idUsuario`)
+    REFERENCES `comercioscr`.`UsuariosEstandar` (`idUsuario`)
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`Parametros`
+-- Table `comercioscr`.`Parametros`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Parametros` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`Parametros` (
   `id` INT NOT NULL,
   `descripcion` VARCHAR(50) NOT NULL,
   `tipo` TINYINT NOT NULL,
@@ -201,9 +201,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`Secciones`
+-- Table `comercioscr`.`Secciones`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Secciones` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`Secciones` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `idComercio` INT NOT NULL,
   `nombre` VARCHAR(50) NOT NULL,
@@ -211,15 +211,15 @@ CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Secciones` (
   INDEX `seccion_fk_idComercio_idx` (`idComercio`),
   CONSTRAINT `seccion_fk_idComercio`
     FOREIGN KEY (`idComercio`)
-    REFERENCES `COMERCIOSCR`.`Comercios` (`idUsuario`)
+    REFERENCES `comercioscr`.`Comercios` (`idUsuario`)
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`Productos`
+-- Table `comercioscr`.`Productos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Productos` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`Productos` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `idComercio` INT NOT NULL,
   `precio`     INT NULL,
@@ -230,15 +230,15 @@ CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`Productos` (
   INDEX `producto_fk_comercio_idx` (`idComercio`),
   CONSTRAINT `producto_fk_comercio`
     FOREIGN KEY (`idComercio`)
-    REFERENCES `COMERCIOSCR`.`Comercios` (`idUsuario`)
+    REFERENCES `comercioscr`.`Comercios` (`idUsuario`)
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`ProductoImagenes`
+-- Table `comercioscr`.`ProductoImagenes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`ProductoImagenes` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`ProductoImagenes` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `idProducto` INT NOT NULL,
   `imagen` VARCHAR(100) NOT NULL,
@@ -246,14 +246,14 @@ CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`ProductoImagenes` (
   INDEX `imagenes_fk_producto_idx` (`idProducto`),
   CONSTRAINT `imagenes_fk_producto`
     FOREIGN KEY (`idProducto`)
-    REFERENCES `COMERCIOSCR`.`Productos` (`id`))
+    REFERENCES `comercioscr`.`Productos` (`id`))
 ENGINE = InnoDB;
 
 DELIMITER //
-CREATE TRIGGER TimagenesMaximo BEFORE UPDATE ON COMERCIOSCR.ProductoImagenes FOR EACH ROW
+CREATE TRIGGER TimagenesMaximo BEFORE UPDATE ON comercioscr.ProductoImagenes FOR EACH ROW
 BEGIN
 	DECLARE cantidad INT;
-	SELECT COUNT(id) INTO cantidad FROM COMERCIOSCR.ProductoImagenes WHERE COMERCIOSCR.ProductoImagenes.idProducto = NEW.idProducto;
+	SELECT COUNT(id) INTO cantidad FROM comercioscr.ProductoImagenes WHERE comercioscr.ProductoImagenes.idProducto = NEW.idProducto;
 	IF(cantidad >= 10) THEN 
 		SIGNAL SQLSTATE '45000' SET message_text = 'Ya llego al maximo de imagenes por producto';
 	END IF;
@@ -261,36 +261,36 @@ END;
 //
 DELIMITER ;
 -- -----------------------------------------------------
--- Table `COMERCIOSCR`.`SeccionesProductos`
+-- Table `comercioscr`.`SeccionesProductos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `COMERCIOSCR`.`SeccionesProductos` (
+CREATE TABLE IF NOT EXISTS `comercioscr`.`SeccionesProductos` (
   `idSeccion` INT NOT NULL,
   `idProducto` INT NOT NULL,
   INDEX `seccion_fk_producto_idx` (`idSeccion`),
   INDEX `producto_fk_seccion_idx` (`idProducto`),
   CONSTRAINT `seccion_fk_producto`
     FOREIGN KEY (`idSeccion`)
-    REFERENCES `COMERCIOSCR`.`Secciones` (`id`),
+    REFERENCES `comercioscr`.`Secciones` (`id`),
   CONSTRAINT `producto_fk_seccion`
     FOREIGN KEY (`idProducto`)
-    REFERENCES `COMERCIOSCR`.`Productos` (`id`))
+    REFERENCES `comercioscr`.`Productos` (`id`))
 ENGINE = InnoDB;
 
 -- Dropeo de usuario, si existe
 
 -- NOTA**** Comentar linea 283 la primera vez que corran el script, descomentar luego de la segunda ejecucion.
 -- -----------------------------------------------------
-DROP USER USUARIOCOMERCIOSCR;
+DROP USER USUARIOcomercioscr;
 -- -----------------------------------------------------
 -- Creacion de USUARIOCAFP
 -- Se crea con la intencion de que solo pueda modificar datos en el alcance del proyecto.
 -- -----------------------------------------------------
-CREATE USER USUARIOCOMERCIOSCR IDENTIFIED BY '123USUARIOCOMERCIOSCR';
+CREATE USER USUARIOcomercioscr IDENTIFIED BY '123USUARIOcomercioscr';
 
 -- -----------------------------------------------------
 -- Asignamos permisos a USUARIOCAFP
 -- -----------------------------------------------------
-GRANT ALL PRIVILEGES ON  COMERCIOSCR.* to USUARIOCOMERCIOSCR;
+GRANT ALL PRIVILEGES ON  comercioscr.* to USUARIOcomercioscr;
 
 
 CALL PAregistrarAdministrador(1, 'jonathanvasquez@gmail.com', 'Jonathan', '123Jonathan', 85602147, 506);
