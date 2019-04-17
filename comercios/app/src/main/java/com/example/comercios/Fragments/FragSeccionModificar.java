b25c606c8e2fe81a9803c92ec607cb47617c90a7
JonathanVM <33185089+JonathanVM@users.noreply.github.com>
2019-04-16 23:49:24 -0600

    Update FragSeccionModificar.java

--- comercios/app/src/main/java/com/example/comercios/Fragments/FragSeccionModificar.java
+++ comercios/app/src/main/java/com/example/comercios/Fragments/FragSeccionModificar.java
@@ -1,7 +1,8 @@
 package com.example.comercios.Fragments;
 
 
-import android.content.Intent;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.text.Editable;
@@ -11,11 +12,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.example.comercios.Global.GlobalComercios;
+import com.example.comercios.Modelo.Seccion;
 import com.example.comercios.Modelo.Util;
 import com.example.comercios.Modelo.VolleySingleton;
 import com.example.comercios.R;
@@ -23,13 +27,17 @@ import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 public class FragSeccionModificar extends Fragment {
 
     private TextInputEditText nombre;
     private TextInputLayout tilNombre;
+    private Seccion seccion = null;
+
     public FragSeccionModificar() {
         // Required empty public constructor
     }
@@ -38,76 +46,120 @@ public class FragSeccionModificar extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.frag_seccion_modificar, container, false);
+        View view = inflater.inflate(R.layout.frag_seccion_modificar, container, false);
         nombre = (TextInputEditText) view.findViewById(R.id.sec_modificar_TextInputEditText);
         tilNombre = (TextInputLayout) view.findViewById(R.id.sec_modificar_TextInputLayout);
         OnclickDelMaterialButtom(view.findViewById(R.id.sec_modificar_MaterialButtonAct));
         OnclickDelMaterialButtom(view.findViewById(R.id.sec_modificar_MaterialButtonEliminar));
+        recuperarSeccion();
         nombre.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 validarDatos();
             }
+
             @Override
-            public void afterTextChanged(Editable s) { }
+            public void afterTextChanged(Editable s) {
+            }
         });
         return view;
     }
 
     public void OnclickDelMaterialButtom(View view) {
 
-        MaterialButton miMaterialButtom = (MaterialButton)  view;
-        miMaterialButtom.setOnClickListener(new View.OnClickListener(){
+        MaterialButton miMaterialButtom = (MaterialButton) view;
+        miMaterialButtom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
 
                     case R.id.sec_modificar_MaterialButtonAct:
-                        if(validarDatos()){
-                            enviarDatos();
+                        if (validarDatos()) {
+                            actualizarSeccion();
                         }
                         break;
 
                     case R.id.sec_modificar_MaterialButtonEliminar:
-
+                        
 
                         break;
-                    default:break; }// fin de casos
+                    default:
+                        break;
+                }// fin de casos
             }// fin del onclick
         });
     }// fin de OnclickDelMaterialButtom
-    private boolean validarDatos(){
+
+    private boolean validarDatos() {
         String dato = nombre.getText().toString();
-        if(dato.length() > 51)
+        if (dato.length() > 51)
             return false;
 
-        if(dato.length() > 0 && dato.length() <= 50 && Util.PATRON_UN_CARACTER_ALFANUMERICO.matcher(dato).find()){
+        if (dato.length() > 0 && dato.length() <= 50 && Util.PATRON_UN_CARACTER_ALFANUMERICO.matcher(dato).find()) {
             tilNombre.setError(null);
             return true;
         }
         tilNombre.setError("Nombre invalido");
         return false;
     }
-    private void enviarDatos() {
+
+    private void actualizarSeccion() {
         //String url = Util.urlWebService + "/seccionActualizar.php?id=" +
-          //      GlobalComercios.getInstance().getIdSecModificar() + "&nombre=" + nombre.getText().toString();
+        //      GlobalComercios.getInstance().getIdSecModificar() + "&nombre=" + nombre.getText().toString();
         String url = Util.urlWebService + "/seccionActualizar.php?id=" +
-               4  + "&nombre=" + nombre.getText().toString();
+                4 + "&nombre=" + nombre.getText().toString();
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
 
                 try {
-                    JSONObject jsonOb = response.getJSONObject("respuesta");
-                    String mensajeError = jsonOb.getString("mensajeError");
-                    if(mensajeError.equalsIgnoreCase("")){
+                    JSONArray jsonA = response.getJSONArray("respuesta");
+                    JSONObject mensajeError = jsonA.getJSONObject(0);
+                    if (mensajeError.getString("mensajeError").equalsIgnoreCase("")) {
                         Mensaje("Exito: Nombre actualizado");
                     } else {
-                        //JSONObject user = jsonOb.getJSONObject("usuario");
+                        Mensaje(mensajeError.getString("mensajeError"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Mensaje("No se puede conectar " + error.toString());
+            }
+        });
+        VolleySingleton.getIntanciaVolley(getActivity().getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+    }
+
+    private void recuperarSeccion() {
+        //String url = Util.urlWebService + "/seccionRecuperar.php?id=" + GlobalComercios.getInstance().getIdSecModificar();
+        String url = Util.urlWebService + "/seccionRecuperar.php?id=" + 4;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    JSONObject jsonOb = response.getJSONObject("datos");
+                    String mensajeError = jsonOb.getString("mensajeError");
+                    if (mensajeError.equalsIgnoreCase("")) {
+                        JSONObject sec = jsonOb.getJSONObject("seccion");
+                        //if(GlobalComercios.getInstance().getComercio().getId() == sec.getInt("idComercio")) {
+                        seccion = new Seccion(sec.getInt("id"), sec.getString("nombre"));
+                        nombre.setText(seccion.getNombre());
+                        //} else {
+                        //Pasar a lista secciones
+                        //}
+                    } else {
                         Mensaje(mensajeError);
+                        remplazoConListaSecciones();
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -123,7 +175,37 @@ public class FragSeccionModificar extends Fragment {
     }
 
 
+    private void eliminarSeccion() {
+        //String url = Util.urlWebService + "/seccionEliminar.php?id=" + GlobalComercios.getInstance().getIdSecModificar();
+        String url = Util.urlWebService + "/seccionEliminar.php?id=" + 4;
 
+        StringRequest stringRequest= new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equalsIgnoreCase("")) {
+                    Mensaje("Exito: Se elimo correctamente");
+                    remplazoConListaSecciones();
+                } else {
+                    Mensaje(response);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Mensaje("No se puede conectar " + error.toString());
+            }
+        });
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS * 2, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        VolleySingleton.getIntanciaVolley(getActivity()).addToRequestQueue(stringRequest);
+    }
+
+    private void remplazoConListaSecciones(){
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        FragSeccionListarComercio  mifrag = new FragSeccionListarComercio ();
+        fragmentTransaction.replace(R.id.menuInferiorComercios_contenido, mifrag, "IdMenuInferior");
+        fragmentTransaction.commit();
+    }
 
     private void Mensaje(String msg){ Toast.makeText(getActivity(), msg,Toast.LENGTH_SHORT).show();};
 
