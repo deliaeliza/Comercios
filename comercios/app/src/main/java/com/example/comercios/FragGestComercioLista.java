6bdc97a62c70adfe42b0eaef164e1408e88b4532
JonathanVM <33185089+JonathanVM@users.noreply.github.com>
2019-04-18 10:07:32 -0600

    listview infinito

---
+++ comercios/app/src/main/java/com/example/comercios/FragGestComercioLista.java
@@ -0,0 +1,29 @@
+package com.example.comercios;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragGestComercioLista extends Fragment {
+
+
+    public FragGestComercioLista() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frag_gest_comercio_lista, container, false);
+    }
+
+}
