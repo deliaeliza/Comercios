26778b6e4bb82876f41efeb2ecad4776bde6161f
Angelo Calderón Barquero <angeelocalderon.17@gmail.com>
2019-05-23 23:00:32 -0600

    j

--- comercios/app/src/main/java/com/example/comercios/Fragments/FragHomeAdmin.java
+++ comercios/app/src/main/java/com/example/comercios/Fragments/FragHomeAdmin.java
@@ -8,10 +8,13 @@ import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.comercios.Global.GlobalAdmin;
 import com.example.comercios.Modelo.Util;
 import com.example.comercios.R;
+import com.google.android.material.card.MaterialCardView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,8 +32,33 @@ public class FragHomeAdmin extends Fragment {
                              Bundle savedInstanceState) {
         mensajeAB(Util.nombreApp);
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.frag_home_admin, container, false);
         GlobalAdmin.getInstance().setVentanaActual(R.layout.frag_home_admin);
-        return inflater.inflate(R.layout.frag_home_admin, container, false);
+
+        MaterialCardView cardViewMapa = (MaterialCardView)view.findViewById(R.id.fHomeAdmin_addAdm);
+        cardViewMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                FragRegAdmin mifrag = new FragRegAdmin ();
+                fragmentTransaction.replace(R.id.contentAdmin, mifrag, "agreagarAdmin");
+                fragmentTransaction.commit();
+            }
+        });
+        MaterialCardView cardViewComercios = (MaterialCardView)view.findViewById(R.id.fHomeAdmin_GestCom);
+        cardViewComercios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                FragGestComercioLista mifrag = new FragGestComercioLista ();
+                fragmentTransaction.replace(R.id.contentAdmin, mifrag, "gestionarComercio");
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
     private void mensajeAB(String msg){((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(msg);};
 }
